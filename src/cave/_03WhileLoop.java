b423b5761106e476ee813c5ae8d6890e66d3b19f
Michal <michal.jarlaczyk@hotmail.com>
2021-05-02 16:27:52 +0200

    While loop lesson

---
+++ src/cave/_03WhileLoop.java
@@ -0,0 +1,38 @@
+package cave;
+
+public class _03WhileLoop {
+	public static void main(String[] args) {
+		System.out.println("Select code, press ctrl+Shift+f - everything will be perfectly formatted.");
+
+		boolean loop = true;
+		boolean loop1 = 4 < 5;
+		boolean loop2 = 6 < 2;
+
+		System.out.println(loop);
+		System.out.println(loop1);
+		System.out.println(loop2);
+		int value = 10;
+		boolean loop3 = value < 20;
+		System.out.println(loop3);
+
+		int condition = 0;
+		
+		while(condition < 30) //while(condition) as long as condition = true, the loop will run
+		{
+			System.out.println("Hello " +condition);
+			
+			condition = condition + 1;
+		}
+		
+		
+		/*
+		while(condition)
+		{
+			
+		}
+		*/
+		
+		
+	}
+
+}
